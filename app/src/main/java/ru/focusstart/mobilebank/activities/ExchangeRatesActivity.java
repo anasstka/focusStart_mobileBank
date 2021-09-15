673ec2400e869d325891b93d6650110d63d1feb1
Anastasia Tsvetkova <nastyushka20020711@gmail.com>
2021-09-15 20:43:15 +0600

    15.09.21 - изменение экрана с курсами валют, добавление возможности ручного обновления данных

---
+++ app/src/main/java/ru/focusstart/mobilebank/activities/ExchangeRatesActivity.java
@@ -0,0 +1,60 @@
+package ru.focusstart.mobilebank.activities;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+import ru.focusstart.mobilebank.DataHelper;
+import ru.focusstart.mobilebank.R;
+import ru.focusstart.mobilebank.adapters.CurrencyAdapter;
+import ru.focusstart.mobilebank.models.Currency;
+import ru.focusstart.mobilebank.repository.PreferencesRepository;
+
+public class ExchangeRatesActivity extends AppCompatActivity {
+
+    ArrayList<Currency> currencies;
+    private CurrencyAdapter currencyAdapter;
+    private ListView listViewCurrencies;
+    private PreferencesRepository preferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exchange_rates);
+
+        preferences = new PreferencesRepository(getApplicationContext());
+
+        listViewCurrencies = findViewById(R.id.lv_currency);
+        View header = getLayoutInflater().inflate(R.layout.listview_header_exchange_rates, null);
+        listViewCurrencies.addHeaderView(header);
+
+        currencies = preferences.getCurrencies();
+        currencyAdapter = new CurrencyAdapter(getApplicationContext(), currencies);
+        listViewCurrencies.setAdapter(currencyAdapter);
+    }
+
+    public void switchActivity(View v) {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void reloadListView(View view) {
+        AsyncTask.execute(() -> {
+            currencies = DataHelper.sendRequest();
+
+            runOnUiThread(() -> {
+                preferences.saveCurrencies(currencies);
+                currencyAdapter.notifyDataSetChanged();
+                listViewCurrencies.setAdapter(currencyAdapter);
+                Toast.makeText(getApplicationContext(), "Данные обновлены!", Toast.LENGTH_SHORT).show();
+            });
+        });
+    }
+}
